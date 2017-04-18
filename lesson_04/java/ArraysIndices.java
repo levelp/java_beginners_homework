da1f0e388450c3189cf03bbc80409fe7b806003c
Denis Stepulenok <super.denis@gmail.com>
2017-04-18 21:27:18 +0300

    Занятие 4 - 2

--- lesson_04/java/ArraysIndices.java
+++ lesson_04/java/ArraysIndices.java
@@ -30,19 +30,6 @@ public class ArraysIndices {
         System.out.println("Arrays.deepToString(A) = " +
                 Arrays.deepToString(A));
 
-        int[][][] A3D = {{{},
-                {3, 4},
-                {7, 2, 1}},
-                {{3, 4}}};
-        // Поиск минимума в 3D-массиве
-        int min = Integer.MIN_VALUE;
-        for (int[][] A2D : A3D)
-            for (int[] A1D : A2D)
-                for (int x : A1D)
-                    if (x < min)
-                        min = x;
-        System.out.println("min = " + min);
-
         // Классы "Расширяемые массивы"
         LinkedList<Integer> ll = new LinkedList<>();
         ArrayList<Integer> al = new ArrayList<>();
